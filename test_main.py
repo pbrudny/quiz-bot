978146f0017f83e40f9e7ebc8608fe42d317e460
Piotr Brudny <pbrudny@gmail.com>
2025-11-21 11:36:29 +0000

    Add tests

--- test_main.py
+++ test_main.py
@@ -1,14 +1,9 @@
-from src.main import main, hello
-
+from main import main, hello
 
 def test_hello():
     assert hello() == "Hello, World!"
 
-
 def test_main_prints_message(capsys):
-    # Run main()
     main()
-
-    # Capture printed output
     captured = capsys.readouterr()
     assert captured.out.strip() == "Hello from quiz-bot!"
